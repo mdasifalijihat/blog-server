1a9c711bda7933bc0cb5f41a6c82bf3a9e3bc47e
mdasifalijihat <mdasifalijihat@gmail.com>
2026-02-05 17:37:49 +0600

    added pagination helper

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
 import { PostStatus } from "../../../generated/prisma/enums";
+import paginationSortingHelper from "../../helper/paginationSortingHelper";
 
 // create post
 const createPost = async (req: Request, res: Response) => {
@@ -42,14 +43,18 @@ const getAllPost = async (req: Request, res: Response) => {
     // author id
     const authorId = req.query.authorId as string | undefined;
 
-    // pagination
-    const page = Number(req.query.page ?? 1);
-    const limit = Number(req.query.limit ?? 10);
-    const skip = (page - 1) * limit;
+    // pagination ata use post paginationsortingHelper
+    // const page = Number(req.query.page ?? 1);
+    // const limit = Number(req.query.limit ?? 10);
+    // const skip = (page - 1) * limit;
 
-    // shoting
-    const sortBy = req.query.sortBy as string | undefined;
-    const sortOrder = req.query.sortOrder as string | undefined;
+    // // shoting
+    // const sortBy = req.query.sortBy as string | undefined;
+    // const sortOrder = req.query.sortOrder as string | undefined;
+
+    const { page, limit, skip, sortBy, sortOrder } = paginationSortingHelper(
+      req.query,
+    );
 
     const result = await postService.getAllPost({
       search: searchString,
